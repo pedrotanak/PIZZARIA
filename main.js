db4d58e84ae72f2f0794f71a25e998d8df45e1f7
pedrotanak <106190056+pedrotanak@users.noreply.github.com>
2022-09-15 17:50:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+menuListaArray = ["Pizza Vegetariana","Pizza de Carne","Pizza de Frango","Pizza de Calabresa"
+                    ];
+
+function getMenu(){
+var htmldata;
+//Completar o código
+}
+
+function addItem(){
+var htmldata;
+var item=document.getElementById("addItem").value;
+//Completar o código
+
+}
+
+function addTop(){
+//Completar o código
+}
